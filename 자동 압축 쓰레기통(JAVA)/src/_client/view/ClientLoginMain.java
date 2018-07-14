c9b0126442895f0656863eaed430e992914cabbd
KimBenGon <rlaqudrhs3@naver.com>
2018-07-14 22:15:03 +0900

    삭제

--- 자동 압축 쓰레기통(JAVA)/src/_client/view/ClientLoginMain.java
+++
@@ -1,7 +0,0 @@
-package _client.view;
-
-public class ClientLoginMain {
-	public static void main(String[] args) {
-		ClientLogin login = new ClientLogin();
-	}
-}
--- 자동 압축 쓰레기통(JAVA)/src/view/Manage.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-import javax.swing.JFrame;
-@SuppressWarnings("serial")
-public abstract class Manage extends JFrame{
-	public Trash_panAb pan[]=new Trash_panAb[50];
-}
